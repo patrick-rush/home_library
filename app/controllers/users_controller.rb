94c5d8598c9f26c1e41ac917e74242996ac13406
patrick-rush <patrick.a.rush@gmail.com>
2020-08-31 18:46:34 -0400

    create new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
 
   # POST: /users
   post "/users" do
+    user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
+    binding.pry
     redirect "/users"
   end
 
