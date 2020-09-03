c171e134c30e5c018b3d47937cda222ba6eb5377
patrick-rush <patrick.a.rush@gmail.com>
2020-09-03 19:33:56 -0400

    update /views/notes names and add note by book id

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,27 +2,47 @@ class NotesController < ApplicationController
 
   # GET: /notes
   get "/notes" do
-    erb :"/notes/index.html"
+    @user = current_user(session)
+    @books = Book.where(user_id: @user.id)
+    @notes = @books.collect do |book|
+      book.notes
+    end
+    # binding.pry
+    erb :"/notes/index"
   end
 
   # GET: /notes/new
-  get "/notes/new" do
-    erb :"/notes/new.html"
+  get "/books/:id/notes/new" do
+    @book = Book.find(params[:id])
+    if logged_in?(session)
+      erb :"/notes/new"
+    else
+      redirect "/login"
+    end
   end
 
   # POST: /notes
   post "/notes" do
+    note = Note.new(content: params[:content], book_id: params[:id])
+    # binding.pry
+    if note.save
+      redirect "/books/#{params[:id]}"
+    else
+      # Add an error here
+      redirect "/books/#{params[:id]}/notes/new"
+    end
+
     redirect "/notes"
   end
 
   # GET: /notes/5
   get "/notes/:id" do
-    erb :"/notes/show.html"
+    erb :"/notes/show"
   end
 
   # GET: /notes/5/edit
   get "/notes/:id/edit" do
-    erb :"/notes/edit.html"
+    erb :"/notes/edit"
   end
 
   # PATCH: /notes/5
