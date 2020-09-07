e2694572136fced5e9292d1218cb27b321132f12
patrick-rush <patrick.a.rush@gmail.com>
2020-09-07 17:29:53 -0400

    clean up notes controller and add errors

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,14 +1,11 @@
 class NotesController < ApplicationController
 
-  # todo - add flash message to notes editor
-
   get "/books/:id/notes/new" do
     redirect_if_not_logged_in
     @book = Book.find(params[:id])
     erb :"/notes/new"
   end
 
-  # POST: /notes
   post "/notes" do
     note = Note.new(content: params[:content], book_id: params[:id])
     if note.save
@@ -17,7 +14,6 @@ class NotesController < ApplicationController
       flash[:error] = "Something went wrong! Please try again."
       redirect "/books/#{params[:id]}/notes/new"
     end
-    redirect "/notes"
   end
 
   get "/notes/:id/edit" do
@@ -25,18 +21,16 @@ class NotesController < ApplicationController
     @note = Note.find(params[:id])
     erb :"/notes/edit"
   end
-  # Fill in notes edit  
 
   patch "/notes/:id" do
     redirect_if_not_logged_in
     note = Note.find(params[:id])
     note.content = params[:content]
     note.save
+    flash[:success] = "Your note has been updated."
     redirect "/books/#{note.book_id}"
   end
-  # Fill in notes id
 
-  # DELETE: /notes/5/delete
   delete "/notes/:id/delete" do
     redirect_if_not_logged_in
     note = Note.find(params[:id])
@@ -44,5 +38,4 @@ class NotesController < ApplicationController
     note.delete
     redirect "/books/#{book_id}"
   end
-  # Add button somewhere
 end
